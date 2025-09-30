54ba50c1bbb28edc3162511b61e72ff7b772a5f2
AMJAD ALI <amjadalitech158@gmail.com>
2025-09-30 18:14:09 +0530

    add js question

---
+++ app.js
@@ -0,0 +1,12 @@
+let x = 10;
+for (let i=1; i<=x; i++) {
+    console.log(i);
+}
+//
+console.log('Print all ODD Number 1 to 15.');
+let num = 15;
+for (let i=1; i<=15; i++) {
+    if (num%2!=0) {
+        console.log(i);
+    }
+}
